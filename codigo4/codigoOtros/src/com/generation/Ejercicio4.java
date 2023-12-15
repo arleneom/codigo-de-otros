1c31cc354a7d0143fd09c896c66cf556b8a3c481
arleneom <arleneomtz@gmail.com>
2023-12-15 17:13:05 -0600

    se editan detalles

--- codigo4/codigoOtros/src/com/generation/Ejercicio4.java
+++ codigo4/codigoOtros/src/com/generation/Ejercicio4.java
@@ -4,12 +4,12 @@ package com.generation;
 import java.util.Scanner;
 /*
  * Por medio de funciones de imprimir en consola y scanner se recaban
- * los datos de desicion de cada jugador.
+ * los datos de decison de cada jugador.
  * Con una condicional if se comparan las respuestas para determinar si son iguales 
  * y dar el mensaje de empate.
  * En caso de no ser asi, se procede con el else a una funcion del tipo 
  * switch para comparar caso por caso las respuestas del jugador 1 con las del 
- * jugador dos, se determina por medio de 3 casos que si el jugador 1 no gano, 
+ * jugador 2, se determina por medio de 3 casos que si el jugador 1 no gano, 
  * entonces gano el jugador 2 esto determinado por el valor del int g 
  * Al final se imprime un mensaje que informa de quien gano concatenando el valor de g
  */
@@ -21,14 +21,14 @@ import java.util.Scanner;
    public static void main(String[] args) { //agregar main
 	
 
-	    Scanner s = new Scanner(System.in);//instroducir el tipo de respuesta que analizara el scanner
+	    Scanner s = new Scanner(System.in);//Indica con System.in el tipo de respuesta que analizará el scanner.
 	    
 	    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
 	    String jugador1 = s.nextLine();//agregar el tipo de input que va a recibir 
 	    
-	    //cambiar a jugador 2 en el print
+	    //cambiar a jugador 2 el mensaje en el print
 	    System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
-      //Scanner s2 = new Scanner(System.in); agregar system.in y comentar para quitarlo porqeu no se esta usando
+      //Scanner s2 = new Scanner(System.in); comentar para quitarlo porqeu no se esta usando
 	    String jugador2 = s.nextLine(); // cambiar el nombre de las variables j1 y j2 
 	    
 	    if (jugador1.equalsIgnoreCase(jugador2)) {
@@ -62,7 +62,7 @@ import java.util.Scanner;
 	      System.out.println("Gana el jugador " + g);
 	    }  
 	  
-	     s.close();
+	     s.close(); // cerrar el scanner 
 	}
    
 	}
