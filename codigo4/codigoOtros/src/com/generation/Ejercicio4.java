4e60fbc37371b07a5cfd66fbf6b35f7cbbfed119
arleneom <arleneomtz@gmail.com>
2023-12-15 16:40:35 -0600

    se agrega descripcion del codigo

--- codigo4/codigoOtros/src/com/generation/Ejercicio4.java
+++ codigo4/codigoOtros/src/com/generation/Ejercicio4.java
@@ -2,6 +2,17 @@
 package com.generation;
 
 import java.util.Scanner;
+/*
+ * Por medio de funciones de imprimir en consola y scanner se recaban
+ * los datos de desicion de cada jugador.
+ * Con una condicional if se comparan las respuestas para determinar si son iguales 
+ * y dar el mensaje de empate.
+ * En caso de no ser asi, se procede con el else a una funcion del tipo 
+ * switch para comparar caso por caso las respuestas del jugador 1 con las del 
+ * jugador dos, se determina por medio de 3 casos que si el jugador 1 no gano, 
+ * entonces gano el jugador 2 esto determinado por el valor del int g 
+ * Al final se imprime un mensaje que informa de quien gano concatenando el valor de g
+ */
 
 //importar scanner 	
 // cambiar el class name a Ejercicio4
